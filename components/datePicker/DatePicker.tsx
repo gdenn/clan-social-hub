991442ed612369b53f15e06cb7eba48ddfcc113c
Dennis Groß <gdennis91@googlemail.com>
2022-05-30 19:57:46 +0200

    Remove old content

--- components/datePicker/DatePicker.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { InputForm } from '../inputGroup/InputForm';
-import styles from './DatePicker.module.css'
-
-export const DatePicker = () => (
-  <div className={styles.datePicker}>
-    <InputForm>Date</InputForm>
-  </div>
-);
